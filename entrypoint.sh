0bbf0534c53f7d224e4e2194498e1aac31aff052
Kazuki Suda <ksuda@zlab.co.jp>
2018-04-06 18:23:31 +0900

    Add a workaround for kubernetes/kubernetes#60741

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e -o pipefail
 
+if [[ "$(kubectl get clusterrolebinding system:node -o="go-template={{.subjects}}")" == "<no value>" ]]; then
+  echo "Deleting clusterrolebinding/system:node. This is a workaround for https://github.com/kubernetes/kubernetes/pull/60741."
+  kubectl delete clusterrolebinding system:node
+fi
+
 if [[ -n "$DEBUG" ]]; then
   exec bash -x "$@"
 fi
