1a7cf4a68fff6acc2f04e9cac0e29c57f9c8e49c
Shinya Uemura <suemura@zlab.co.jp>
2020-01-07 13:24:24 +0900

    Update dependencies for Kubernetes 1.17.0

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -4,11 +4,11 @@ set -e -o pipefail; [[ -n "$DEBUG" ]] && set -x
 
 dependencies=(
   "github.com/spf13/cobra@v0.0.5" \
-  "k8s.io/apimachinery@kubernetes-1.16.0" \
-  "k8s.io/cli-runtime@kubernetes-1.16.0" \
-  "k8s.io/client-go@kubernetes-1.16.0" \
-  "k8s.io/component-base@kubernetes-1.16.0" \
-  "k8s.io/kubectl@kubernetes-1.16.0" \
+  "k8s.io/apimachinery@v0.17.0" \
+  "k8s.io/cli-runtime@v0.17.0" \
+  "k8s.io/client-go@v0.17.0" \
+  "k8s.io/component-base@v0.17.0" \
+  "k8s.io/kubectl@v0.17.0" \
 )
 
 GO111MODULE=on go get "${dependencies[@]}"
